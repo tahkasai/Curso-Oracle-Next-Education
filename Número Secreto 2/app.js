ae83d542241c9c26cb9d9c0a3a3c2715e770c8ae
Tainá Kasai <tainakasai01@gmail.com>
2025-09-16 00:04:28 -0300

    update project

--- Número Secreto 2/app.js
+++ Número Secreto 2/app.js
@@ -9,7 +9,14 @@ let tentivas = 1;
 function exibirTextoNaTela(tag,texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 // função pra mensagem inicial
