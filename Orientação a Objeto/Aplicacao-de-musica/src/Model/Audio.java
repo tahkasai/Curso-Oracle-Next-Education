8567441edfc7769c28de561a4fe4ac256431f5c9
tahkasai <tainakasai01@gmail.com>
2026-02-04 19:28:41 -0300

    feat: Add superclass Audio
    Adiciona classe modelo de Audio

---
+++ Orientação a Objeto/Aplicacao-de-musica/src/Model/Audio.java
@@ -0,0 +1,61 @@
+package Model;
+
+public class Audio {
+    private String title;
+    private int duration; // seconds
+    private int reproductions; // total reproductions
+    private int likes; // total likes
+    private int classification;
+
+    // Construtor
+    public Audio() {
+
+    }
+
+    public Audio(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    // Setters
+    public Audio setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Audio setDuration(int duration) {
+        this.duration = duration;
+        return this;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getReproductions() {
+        return reproductions;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getClassification() {
+        return classification;
+    }
+
+    // Methods
+    public void like(){
+        this.likes++;
+    }
+
+    public void reproduction(){
+        this.reproductions++;
+    }
+
+}
